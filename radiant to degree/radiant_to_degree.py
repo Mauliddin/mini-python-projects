60ff88cda03837b783af4e663154f10f768a96b3
Mauliddin <41766164+Mauliddin@users.noreply.github.com>
2024-05-13 14:23:09 +0800

    Update radiant_to_degree.py

--- radiant to degree/radiant_to_degree.py
+++ radiant to degree/radiant_to_degree.py
@@ -1,9 +1,13 @@
+import math
+
 def convert(radiant):
-  return radiant * (180/3.14)
+    return radiant * (180/math.pi)
+
 
 def main():
-  usr_input = input("Enter Radiant : ")
-  if usr_input.isnumeric():
-    print(f"{usr_input} Radiant = {convert(int(usr_input)} degree")
+    usr_input = input("Enter Radiant : ")
+    if usr_input.isnumeric():
+        in_degree = convert(int(usr_input))
+        print(f"{usr_input} Radiant = {round(in_degree, 2)} degree")
 
 main()
